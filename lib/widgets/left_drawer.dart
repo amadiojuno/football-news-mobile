adac0e9bbb2b45fa2a28cbb11ffa1f66cb827ff8
Amadio Juno <amadio.juno@ui.ac.id>
2025-11-12 14:35:01 +0700

    Tutorial 8 selesai

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
 // TODO: Impor halaman NewsFormPage jika sudah dibuat
 import 'package:football_news/screens/newslist_form.dart';
+import 'package:football_news/screens/news_entry_list.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -69,16 +70,15 @@ class LeftDrawer extends StatelessWidget {
           ),
           // TODO: Buat ListTile baru untuk ke halaman melihat news
           ListTile(
-            leading: const Icon(Icons.article_outlined),
-            title: const Text('View News'),
-            // Bagian redirection ke NewsListPage
-            // onTap: () {
-            //   Navigator.push(
-            //       context,
-            //       MaterialPageRoute(
-            //         builder: (context) => NewsListPage(),
-            //       ));
-            // },
+              leading: const Icon(Icons.add_reaction_rounded),
+              title: const Text('News List'),
+              onTap: () {
+                  // Route to news list page
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const NewsEntryListPage()),
+                  );
+              },
           ),
         ],
       ),
